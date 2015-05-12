01b00765b13bad6d1e0b8578c7dce6775237c7f8
katerinako <katerinakcv@gmail.com>
2015-05-12 14:55:42 +0000

    added image button

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,14 @@ class Recipe < ActiveRecord::Base
     validates :name, presence: true, length: {minumum: 5, maximum: 100}
     validates :summarize, presence: true, length: {minumum: 10, maximum: 150}
     validates :description, presence: true, length: {minumum: 20, maximum: 500}
+    mount_uploader :picture, PictureUploader
+    validate :picture_size
+    
+    private
+        def picture_size
+            if picture.size > 5.megabytes
+                errors.add(:picture, "Should be less than 5MB" )
+            end
+        end
     
 end
